484db1215ed629e9331632ba3b1e7f18c8377556
Matheus Aparecido <121892469+mathasilv@users.noreply.github.com>
2025-11-28 00:58:25 +0000

    [HAL] feat: LoRaHal.h stub completo (CubeSat driver)

--- lib/Drivers/LoRaHal/LoRaHal.h
+++ lib/Drivers/LoRaHal/LoRaHal.h
@@ -3,6 +3,14 @@
 
 #pragma once
 
+#include "HAL/interface/SPI.h"
+#include "HAL/interface/GPIO.h"
+
 namespace Drivers {
-    class LoRaHal;
+    class LoRaHal {
+    public:
+        LoRaHal(HAL::SPI* spi, HAL::GPIO* gpio);
+        bool begin(uint32_t frequency);
+        // ... métodos SX1276
+    };
 }
